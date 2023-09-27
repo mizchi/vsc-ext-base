e25355d3ea397af6ceb9a1fc502c7190d288711b
mizchi <miz404@gmail.com>
2023-09-27 12:58:47 +0900

    update

--- test/index.ts
+++ test/index.ts
@@ -1,25 +1,28 @@
-import path from 'node:path';
+import path from "node:path";
 
-import { glob } from 'glob';
-import Mocha from 'mocha';
+import { glob } from "glob";
+import Mocha from "mocha";
 
 /**
  * !: must be synchronized
  */
-export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
-    const mocha = new Mocha({ color: true });
+export function run(
+  testsRoot: string,
+  cb: (error: any, failures?: number) => void,
+): void {
+  const mocha = new Mocha({ color: true });
 
-    glob('**/**.test.js', { cwd: testsRoot })
-        .then((files) => {
-            for (const f of files) mocha.addFile(path.resolve(testsRoot, f));
+  glob("**/**.test.js", { cwd: testsRoot })
+    .then((files) => {
+      for (const f of files) mocha.addFile(path.resolve(testsRoot, f));
 
-            try {
-                mocha.run((failures) => {
-                    cb(null, failures);
-                });
-            } catch (error) {
-                cb(error);
-            }
-        })
-        .catch((error) => cb(error));
+      try {
+        mocha.run((failures) => {
+          cb(null, failures);
+        });
+      } catch (error) {
+        cb(error);
+      }
+    })
+    .catch((error) => cb(error));
 }
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,17 +1,17 @@
-import { strictEqual } from 'node:assert';
+import { strictEqual } from "node:assert";
 
-import vscode from 'vscode';
+import vscode from "vscode";
 
-describe('#test sample', () => {
-    before(() => {
-        vscode.window.showInformationMessage('Test begin!');
-    });
+describe("#test sample", () => {
+  before(() => {
+    vscode.window.showInformationMessage("Test begin!");
+  });
 
-    it('one plus one equals two', () => {
-        strictEqual(2, 1 + 1);
-    });
+  it("one plus one equals two", () => {
+    strictEqual(2, 1 + 1);
+  });
 
-    after(() => {
-        vscode.window.showInformationMessage('Test end!');
-    });
+  after(() => {
+    vscode.window.showInformationMessage("Test end!");
+  });
 });
